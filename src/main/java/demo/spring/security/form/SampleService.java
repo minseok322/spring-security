5fbbb931b407caeae89646cc89499102beb1b2dc
kms322 <kms970322@gmail.com>
2025-02-20 18:20:40 +0900

    filterchain

--- src/main/java/demo/spring/security/form/SampleService.java
+++ src/main/java/demo/spring/security/form/SampleService.java
@@ -1,8 +1,9 @@
 package demo.spring.security.form;
 
-import demo.spring.security.account.Account;
-import demo.spring.security.account.AccountContext;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Log4j2
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Service;
 public class SampleService {
 
     public void dashboard() {
-        Account account = AccountContext.getAccount();
-        log.info(account.getUsername());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        log.info(userDetails.getUsername());
     }
 }
